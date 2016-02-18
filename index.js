6f9f401efe68fe060fdfdc2d2dc7cad8dc847652
Scott Christensen <scottmc526@gmail.com>
2016-02-18 15:11:12 -0700

    user can post

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ var app = angular.module('scott', []);
 app.controller('control', function($scope){
   $scope.showForm = false;
   $scope.posts = [];
-  
+  $scope.submitPost = function(){
+    var post = {};
+    post.title = $scope.title;
+    post.author = $scope.author;
+    post.url = $scope.url;
+    post.description = $scope.description;
+    $scope.posts.push(post);
+  }
 })
